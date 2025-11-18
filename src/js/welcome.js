f8ecaac9e82eb98a4871fe722c33d6070a3064bc
vizavisual <viza4rt@gmail.com>
2025-11-18 10:38:50 +0700

    Update welcome.js

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -15,7 +15,7 @@ export const welcome = () => {
         return `
             <img src="${coupleImage}" alt="couple animation">
             <figcaption>
-                ${brideLName.firstName = fullName.join(' ')} x ${bridePName.split(' ')[0]}
+                ${brideLName.firstName = brideLName.join(' ')} x ${bridePName.split(' ')[0]}
             </figcaption>`;
     };
 
